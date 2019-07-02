33e287860a82abb3cdd719e4eabf27a9015d87fe
Santiago Gómez <sag.gomez@gmail.com>
2019-07-02 09:18:29 -0300

    Fix custom css for VSCodium, removed image from lock script

--- .config/scripts/lock.sh
+++ .config/scripts/lock.sh
@@ -4,10 +4,10 @@ scrot /tmp/screen.png
 
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
-if [ -z "$f1" ] 
-  then
-    convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-fi
+# if [ -z "$f1" ] 
+#  then
+#    convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+# fi
 
 i3lock -u -i /tmp/screen.png
 
